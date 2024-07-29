10f4e5598242ce0600634b0db719587e38f1fe4d
dipesh <dipeshsth00@gmail.com>
2024-07-29 19:04:44 +0545

    day 13 and day 14 complete

---
+++ day 13/debug_exercise_2.py
@@ -0,0 +1,29 @@
+# # error code
+
+# year = input("Which year do you want to check?")
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+# # debuged code
+
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
