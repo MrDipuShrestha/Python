d7f800b90c92fb0ff62f273b571f0e03635eb9b5
dipesh <dipeshsth00@gmail.com>
2024-09-19 09:15:00 +0545

    Working with api

---
+++ day_33_Working_with_API/main.py
@@ -0,0 +1,52 @@
+import requests
+from datetime import datetime
+import smtplib
+
+MY_LAT = 27.671022
+MY_LNG = 85.429817
+
+my_email = "dipstha393@gmail.com"
+password = "hkct tuav brka zmzv"
+
+
+def is_above_me():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+
+    data = response.json()
+
+    longitude = float(data["iss_position"]["longitude"])
+    latitude = float(data["iss_position"]["latitude"])
+
+    if MY_LAT - 5 <= latitude <= MY_LAT + 5 and MY_LNG - 5 <= longitude <= MY_LNG + 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LNG,
+        "formatted": 0,
+    }
+
+    response = requests.get(url=" https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now <= sunrise or time_now >= sunset:
+        return True
+
+
+if is_above_me() and is_night():
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=my_email,
+                            msg="Subject:Look up👆\n\n The iss is right above you."
+                            )
